ed2c10672281f21b2f41d557b5a689afcc82d6c2
Sylvestre <sylvestre.gug@gmail.com>
2015-08-07 17:08:14 -0400

    Font and Design

--- f_tests/tests.py
+++ f_tests/tests.py
@@ -11,7 +11,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -34,14 +34,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.find_element_by_css_selector('#chartdiv > .amcharts-main-div')
 
         #He scroll down a little bit further and keeps reading about the number of votes per yer.
+        sub_title = self.browser.find_element_by_css_selector('#metrics_2 > .row > div > h2')
+        self.assertEqual('NJ Votes in Years', sub_title.text)
         chart = self.browser.find_element_by_class_name('v_p_y')
+
         #He noticed a chart in this section.
-        self.browser.find_element_by_css_selector('.v_p_y.amcharts-main-div')
+        self.browser.find_element_by_css_selector('#chartdiv2 > .amcharts-main-div')
 
         #He finally spend some time analyzing the pie chart on vote repartition per side (Democrate/Republicans/UNA)
+        sub_title = self.browser.find_element_by_css_selector('#metrics_3 > .row > div > h2')
+        self.assertEqual('What about the party ?', sub_title.text)
         chart = self.browser.find_element_by_class_name('v_p_p')
         #He noticed a chart in this section.
-        self.browser.find_element_by_css_selector('.v_p_p.amcharts-main-div')
+        self.browser.find_element_by_css_selector('#chartdiv3 > .amcharts-main-div')
 
 
         self.fail('Finish the test!')
